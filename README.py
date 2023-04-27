8d29f81ec28341cf2b11ebe90c0b421cb0456f01
gaffeyyu <131973902+gaffeyyu@users.noreply.github.com>
2023-04-27 20:42:44 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,15 @@
-import time 
+import time
+
+def pomodoro_timer():
+    print("开始专注时间！")
+    for i in reversed(range(1, 26)):
+        minutes = i // 60
+        seconds = i % 60
+        print(f"还有 {minutes:02d}:{seconds:02d}...")
+        time.sleep(1)
+    print("休息时间到了！")
+
 while True:
-current_time = 
-time.strftime("%H:%M:%S")
-current_date =
-time.strftime("%Y-%m-%d")
-print("Current Time:", current_time)
-print（"Current Date:", current_date)
-time.sleep(1)
+    pomodoro_timer()
+    print("休息一下，开始下一个番茄时间。")
+    time.sleep(300)  # 休眠5分钟，以便专注一段时间后再次集中注意力
